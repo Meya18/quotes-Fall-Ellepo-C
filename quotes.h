613a9e5b2f53aa406281278e485391c45fb17731
LinElp <ellepolindsay2@gmail.com>
2025-03-28 11:51:25 +0100

    Initial quotes program

---
+++ quotes.h
@@ -0,0 +1,19 @@
+//
+// Created by ellep on 28/03/2025.
+//
+
+#ifndef QUOTES_FALL_ELLEPO_C_QUOTES_H
+#define QUOTES_FALL_ELLEPO_C_QUOTES_H
+
+// quotes.h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_QUOTES 10
+
+void print_quote(char ** quotes, int index);
+void print_random_quote(char ** quotes);
+
+#endif //QUOTES_FALL_ELLEPO_C_QUOTES_H
